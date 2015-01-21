ad8e3087b62aa90e06d490e6b2b84a23ca5c272a
yamadapc <tacla.yamada@gmail.com>
2015-01-21 03:46:17 -0200

    Add a very nasty solution for having ETA's
    Also, patch the tests. The nastiness here is mostly caused by our
    terrible state handling and stats gathering / passing along. I know it
    leaves a lot of refactoring pending with some code duplication and long
    expressions. But I'm happy that we now have an skeleton on which to
    work on. After all, this already works.

--- lib/System/Console/AsciiProgress.hs
+++ lib/System/Console/AsciiProgress.hs
@@ -13,8 +13,8 @@ module System.Console.AsciiProgress
   where
 
 import Control.Applicative ((<$>))
-import Control.Concurrent (Chan, MVar, modifyMVar, newChan, newMVar, readChan,
-                           readMVar, writeChan)
+import Control.Concurrent -- (Chan, MVar, modifyMVar, newChan, newEmptyMVar,
+                           -- newMVar, readChan, readMVar, writeChan, modifyMVar_)
 import Control.Concurrent.Async (Async, async, poll)
 import Control.Monad (when)
 import Data.Default (Default(..))
@@ -22,13 +22,14 @@ import Data.Maybe (isJust)
 import Data.List.Utils (replace)
 import Data.Time.Clock
 import Text.Printf
-import System.Console.ANSI (setCursorColumn)
+import System.Console.ANSI (clearLine, setCursorColumn)
 import System.IO (BufferMode(..), hSetBuffering, stdout)
 
 data ProgressBar = ProgressBar { pgFuture  :: Async ()
                                , pgChannel :: Chan (Maybe Int)
                                , pgOptions :: Options
                                , pgCompleted :: MVar Int
+                               , pgLastTick :: MVar UTCTime
                                }
   deriving(Eq)
 
@@ -53,43 +54,55 @@ newProgressBar :: Options -> IO ProgressBar
 newProgressBar opts = do
     hSetBuffering stdout NoBuffering
     mcompleted <- newMVar 0
+    mlastTick <- newEmptyMVar
     chan <- newChan
     initTime <- getCurrentTime
-    future <- async $ start mcompleted chan initTime
-    return $ ProgressBar future chan opts mcompleted
+    future <- async $ start mlastTick mcompleted chan initTime
+    return $ ProgressBar future chan opts mcompleted mlastTick
   where
-    start mcompleted chan initTime = do
+    start mlastTick mcompleted chan initTime = do
         c <- readMVar mcompleted
         when (c < pgTotal opts) $ do
             readChan chan >>= handleMessage
-            start mcompleted chan initTime
+            start mlastTick mcompleted chan initTime
+        clearLine
+        setCursorColumn 0
       where
         handleMessage (Just n) = do
-            c <- modifyMVar mcompleted (\c -> let c' = c + n in return (c', c'))
-            render initTime opts c
+            completed <- modifyMVar mcompleted (\c -> let c' = c + n in return (c', c'))
+            currentTime <- getCurrentTime
+            isFirstTick <- isEmptyMVar mlastTick
+            if isFirstTick
+                then do
+                    putMVar mlastTick currentTime
+                    render currentTime initTime opts completed
+                else do
+                    lastTick <- swapMVar mlastTick currentTime
+                    render lastTick initTime opts completed
         handleMessage Nothing = handleMessage (Just 1)
 
-render :: (PrintfArg a, Integral a)
-       => UTCTime -> Options -> a -> IO ()
-render initTime opts completed = do
+render :: UTCTime -> UTCTime -> Options -> Int -> IO ()
+render lastTick initTime opts completed = do
     currentTime <- getCurrentTime
-    let progressStr = getProgressStr currentTime initTime opts completed
+    let progressStr = getProgressStr lastTick currentTime initTime opts completed
+    clearLine
     setCursorColumn 0
     putStr progressStr
 
-getProgressStr :: (PrintfArg a, Integral a)
-               => UTCTime -> UTCTime -> Options -> a -> String
-getProgressStr currentTime initTime opts completed =
+getProgressStr :: UTCTime -> UTCTime -> UTCTime -> Options -> Int -> String
+getProgressStr lastTick currentTime initTime opts completed =
     let fmt = pgFormat opts
         elapsed = diffUTCTime currentTime initTime
         percent = floor (100 * (fromIntegral completed :: Double) /
                          fromIntegral (pgTotal opts)) :: Int
+        eta = getEta completed (pgTotal opts) lastTick currentTime
         -- Use TJ Holowaychuk's approach
         tmpStr = replaceMany [ (":elapsed", printf "%3.1f"
                                             (realToFrac elapsed :: Double))
                              , (":current", printf "%3d" completed)
                              , (":total", printf "%3d" (pgTotal opts))
                              , (":percent", printf "%3d%%" percent)
+                             , (":eta", printf "%3.1f" eta)
                              ]
                              fmt
         barWidth = pgWidth opts - length (replace ":bar" "" tmpStr)
@@ -104,6 +117,13 @@ getProgressStr currentTime initTime opts completed =
                                      pairs
     percentCompleted = (fromIntegral completed :: Double) /
                        fromIntegral (pgTotal opts)
+
+getEta :: Integral a => a -> a -> UTCTime -> UTCTime -> Double
+getEta completed total lastTick currentTime = remaining / secondsPerTick
+  where
+    secondsSinceLastTick = realToFrac (diffUTCTime currentTime lastTick)
+    secondsPerTick = fromIntegral total * secondsSinceLastTick
+    remaining = fromIntegral (total - completed)
 
 complete :: ProgressBar -> IO Bool
 complete pg = isJust <$> poll (pgFuture pg)
--- test/System/Console/AsciiProgressSpec.hs
+++ test/System/Console/AsciiProgressSpec.hs
@@ -1,33 +1,43 @@
 module System.Console.AsciiProgressSpec where
 
 import System.Console.AsciiProgress
+import Data.Time.Clock
 import Test.Hspec
 
 spec :: Spec
 spec =
     describe "getProgressStr :: Options -> Int -> String" $ do
         it "fills the completed percentage of the total width" $ do
+            initTime <- getCurrentTime
+            currentTime <- getCurrentTime
+            lastTick <- getCurrentTime
             let opts = def { pgTotal = 100
                            , pgWidth = 100
                            , pgFormat = ":bar"
                            }
-            getProgressStr opts 10 `shouldBe`
+            getProgressStr lastTick currentTime initTime opts 10 `shouldBe`
                 "==========                                                                                          "
 
         it "replaces `:bar` in the `pgFormat` format `String`" $ do
+            initTime <- getCurrentTime
+            currentTime <- getCurrentTime
+            lastTick <- getCurrentTime
             let opts = def { pgTotal = 100
                            , pgWidth = 100
                            , pgFormat = "working [:bar]"
                            }
-            getProgressStr opts 10 `shouldBe`
+            getProgressStr lastTick currentTime initTime opts 10 `shouldBe`
                 "working [=========                                                                                 ]"
 
         it "uses the options' completed and pending characters" $ do
+            initTime <- getCurrentTime
+            currentTime <- getCurrentTime
+            lastTick <- getCurrentTime
             let opts = def { pgTotal = 100
                            , pgWidth = 100
                            , pgCompletedChar = '*'
                            , pgPendingChar = '-'
                            , pgFormat = ":bar"
                            }
-            getProgressStr opts 10 `shouldBe`
+            getProgressStr lastTick currentTime initTime opts 10 `shouldBe`
                 "**********------------------------------------------------------------------------------------------"

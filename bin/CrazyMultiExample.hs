f2a1f904e4e03e7779231c6b36809c06d51f1beb
yamadapc <tacla.yamada@gmail.com>
2015-11-09 13:44:03 -0200

    Better illustration of crazy-multi-example

--- bin/CrazyMultiExample.hs
+++ bin/CrazyMultiExample.hs
@@ -8,6 +8,7 @@ main :: IO ()
 main = displayConsoleRegions $ do
     fs <- forM ([0..100] :: [Int]) $ \i -> do
         pg <- newProgressBar def { pgWidth = 100
+                                 , pgFormat = "pg " ++ show i ++ " " ++ (pgFormat def)
                                  , pgOnCompletion = Just $ "pg " ++ show i ++ " is Done!"
                                  }
         speed <- (* 100000) <$> randomRIO (3, 10)
